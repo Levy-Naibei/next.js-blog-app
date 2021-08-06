4bf74e41a172ac34689677b6a8f06dd4d6810d27
Levy-Naibei <levynaibei2@mail.com>
2021-08-06 12:13:29 +0300

    feat: added layout component and css module

---
+++ pages/posts/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+const posts = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>first post</title>
+            </Head>
+        </Layout>
+    )
+}
+
+export default posts;
